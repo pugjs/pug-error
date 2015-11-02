8762ad072651318aac240553f066009630ef4e22
Timothy Gu <timothygu99@gmail.com>
2015-11-02 14:57:16 -0800

    Add test coverage for the last commit

--- test/index.js
+++ test/index.js
@@ -48,13 +48,13 @@ test('without source', function () {
 
 test('with column', function () {
   test('and with a filename', function () {
-    var err = error('MY_CODE', 'My message', {line: 3, column: 1, filename: 'myfile'});
-    assert(err.message === 'myfile:3:1\n\nMy message');
+    var err = error('MY_CODE', 'My message', {line: 3, column: 2, filename: 'myfile', src: 'foo\nbar\nbaz\nbash\nbing'});
+    assert(err.message === 'myfile:3:2\n    1| foo\n    2| bar\n  > 3| baz\n--------^\n    4| bash\n    5| bing\n\nMy message');
     assert(err.code === 'JADE:MY_CODE');
     assert(err.msg === 'My message');
     assert(err.line === 3);
     assert(err.filename === 'myfile');
-    assert(err.src === undefined);
+    assert(err.src === 'foo\nbar\nbaz\nbash\nbing');
   });
   test('and with no filename', function () {
     var err = error('MY_CODE', 'My message', {line: 3, column: 1});
